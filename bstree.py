619b9918faa9ce36d61ff8574dfeb62866f6837c
Gideon VanRiette <gvanriette@gmail.com>
2014-04-13 15:02:08 -0500

    Basic outline of classes and methods

---
+++ bstree.py
@@ -0,0 +1,21 @@
+class BSTree:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, key, value=None):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def search(self, key):
+        pass
+
+class Node:
+    def __init__(self, key, value, left=None, right=None):
+        self.key = key
+        self.payload = value
+        self.left = left
+        self.right = right
+
+
---
+++ tests.py
@@ -0,0 +1,1 @@
+from bstree import BSTree, Node
